11961ecf8c0a8cca2297e15717e456b941a1cfd8
travis <trvvisny2000@gmail.com>
2020-03-31 19:53:47 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-makepanda\makepanda.bat --everything --installer --msvc-version=14.2 --windows-sdk=10 --no-eigen --threads=2
+makepanda\makepanda.bat --everything --installer --msvc-version=14.2 --windows-sdk=10 --no-eigen --threads=4
 pause
